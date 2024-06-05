bdf4b7a252a10f1a0ce7d4ecb82cf67baa6a2fbc
dhaval <dhaval.vaghela@einfochips.com>
2024-06-05 14:27:56 +0530

    df

--- settings/connect_db.py
+++ settings/connect_db.py
@@ -10,7 +10,7 @@ password = os.environ.get("DBPASSWORD")
 
 def connect():
     credential = ManagedIdentityCredential(client_id=os.environ.get("AZURE_CLIENT_ID"))
-    token = credential.get_token("https://ossrdbms-aad.database.windows.net/.default")
+    token = credential.get_token("https://ossrdbms-aad.database.windows.net/.default").token
     print(f"generated token is {token}")
     conn_string = "host={0} user={1} dbname={2} password={3}".format(host, user, dbname, token)
 
