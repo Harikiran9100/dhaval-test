e46425bb9e3db3a8b5fefd0f402991087accf2b1
dhaval <dhaval.vaghela@einfochips.com>
2024-06-05 14:48:58 +0530

    df

--- settings/connect_db.py
+++ settings/connect_db.py
@@ -9,11 +9,11 @@ password = os.environ.get("DBPASSWORD")
 
 
 def connect():
-    credential = ManagedIdentityCredential(client_id=os.environ.get("AZURE_CLIENT_ID"))
+    credential = ManagedIdentityCredential(client_id=os.environ.get("AZURE_CLIENT_ID"),tenant_id=os.environ.get("AZURE_TENANT_ID"))
     token = credential.get_token("https://ossrdbms-aad.database.windows.net/.default").token
     print(f"generated token is {token}")
-    conn_string = "host={0} user={1} dbname={2} password={3}".format(host, user, dbname, token)
-
+    #conn_string = "host={0} user={1} dbname={2} password={3}".format(host, user, dbname, token)
+    conn_string = "host='{}' dbname='{}' user='{}' password='{}' sslmode='require'".format(host, dbname, credential.get_token('https://ossrdbms-aad.database.windows.net/.default').token, credential.get_token('https://ossrdbms-aad.database.windows.net/.default').token)
     conn = psycopg2.connect(conn_string)
 
 
